96bb202739534ed7167cacef837172ba16259868
sahithyan2701@gmail.com <sahithyan2701@gmail.com>
2019-01-07 20:39:56 +0530

    Coding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,34 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddPage } from '../pages/add/add';
+import { DetailsPage } from '../pages/details/details';
+import { EditPage } from '../pages/edit/edit';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+		AddPage,
+		DetailsPage,
+		EditPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+		IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+		AddPage,
+		DetailsPage,
+		EditPage
   ],
   providers: [
     StatusBar,
--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the AddPage page.
@@ -14,12 +15,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add.html',
 })
 export class AddPage {
+	title: string;
+	date: string;
+	time: string;
+	description: string;
+	public count_downs;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController, public storage: Storage) {
   }
+
+  getdata = this.storage.get('counting').then((value) => {
+  	if (value == null) {
+  		value = [];
+  	}
+  	this.count_downs = value;
+  });
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPage');
   }
+
+	saveItem() {
+		let newCD = {
+			"id" : (this.count_downs.length + 1),
+			"title" : this.title,
+			"date" : this.date,
+			"time" : this.time,
+			"description" : this.description
+		};
+		this.count_downs.push(newCD);
+
+		this.storage.set('counting', this.count_downs);
+
+		this.navCtrl.pop();
+		location.reload();
+	}
 
 }
